bab384dfa5027b36ebc560f6f58470fda8702d39
DuCrato <williamanderson1994@hotmail.com>
2021-11-08 15:03:44 -0300

    App executável

--- app/src/main/java/com/example/imcdioprojeto1/MainActivity.kt
+++ app/src/main/java/com/example/imcdioprojeto1/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.imcdioprojeto1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 import com.example.imcdioprojeto1.databinding.ActivityMainBinding
+import java.text.DecimalFormat
+import android.content.Intent as Intent1
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,29 +18,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonCalcular.setOnClickListener{calcularIMC()}
+        binding.buttonCalcular.setOnClickListener{ calcularIMC() }
     }
 
     fun calcularIMC(){
-        val campoAltura = binding.editTextAltura.text.toString()
-        val campoPeso   = binding.editTextPeso.text.toString()
+
+        val campoAltura = binding.alturaCampo.text.toString()
+        val campoPeso   = binding.pesoCampo.text.toString()
         val altura = campoAltura.toDoubleOrNull()
         val peso   = campoPeso.toDoubleOrNull()
 
         if(altura == null || peso == null){
 
-            if(altura == null && peso != null){
-                Toast.makeText(this,"Campo Altura esta vazio!!!",Toast.LENGTH_SHORT).show()
-            }else if(peso == null && altura != null){
-                Toast.makeText(this,"Campo Peso esta vazio!!!.",Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(this,"Altura e Peso estão vazios!!!",Toast.LENGTH_SHORT).show()
-            }
-            return
-        }
-        
-        var resultadoIMC = peso!! / (altura!! * altura)
-        val formatResultado = format(resultadoIMC)
+            if(altura == null && peso != null){ Toast.makeText(this,"Campo Altura esta vazio!!!",Toast.LENGTH_SHORT).show()
+            }else if(peso == null && altura != null){ Toast.makeText(this,"Campo Peso esta vazio!!!.",Toast.LENGTH_SHORT).show()
+            }else{ Toast.makeText(this,"Altura e Peso estão vazios!!!",Toast.LENGTH_SHORT).show() }
 
+            return
+        }else{
+
+            val intencao = Intent1(this, ResultadoActivity::class.java)
+
+            intencao.putExtra("altura", altura)
+            intencao.putExtra("peso", peso)
+
+            startActivity(intencao)
+        }
     }
 }
--- app/src/main/java/com/example/imcdioprojeto1/ResultadoActivity.kt
+++ app/src/main/java/com/example/imcdioprojeto1/ResultadoActivity.kt
@@ -1,11 +1,52 @@
 package com.example.imcdioprojeto1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import java.text.DecimalFormat
 
 class ResultadoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val btResetar: Button = findViewById(R.id.buttonResetar)
+
+        exibirResultado()
+        btResetar.setOnClickListener{telaMain()}
+
+    }
+
+    fun exibirResultado(){
+
+        val dadoMain = this.intent.extras
+
+        var altura = dadoMain?.getDouble("altura")
+        var peso   = dadoMain?.getDouble("peso")
+
+        var resultadoImc = peso!! / (altura!! * altura)
+        val numero = DecimalFormat("#.00")
+        var resultadoFinal = numero.format(resultadoImc)
+
+        val mostrarResultado: TextView = findViewById(R.id.text_resultado_imc)
+        val tipoDeGordura:    TextView = findViewById(R.id.text_tipo_corpo)
+
+        mostrarResultado.text = (resultadoFinal.toString())
+
+        if(resultadoImc < 18.6){
+            tipoDeGordura.text = ("Magreza")
+        }else if(resultadoImc < 25.0){
+            tipoDeGordura.text = ("Normal")
+        }else if(resultadoImc < 30.0){
+            tipoDeGordura.text = ("Sobre Peso")
+        }else{ tipoDeGordura.text = ("Obesidade")}
+    }
+
+    fun telaMain(){
+        val intencao = Intent(this, MainActivity::class.java)
+        startActivity(intencao)
     }
 }
